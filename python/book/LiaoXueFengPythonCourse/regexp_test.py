2b415cccde8e708fdd56839ee0a4916e61448be4
Junctionzc <junctionzc@gmail.com>
2016-04-14 21:56:35 +0800

    learn python day 7

---
+++ python/book/LiaoXueFengPythonCourse/regexp_test.py
@@ -0,0 +1,34 @@
+import re
+print re.match(r'^\d{3}\-\d{3,8}$', '010-12345')
+print re.match(r'^\d{3}\-\d{3,8}$', '010 12345')
+
+test = '010-12345'
+if re.match(r'^\d{3}\-\d{3,8}$', test):
+    print 'ok'
+else:
+    print 'failed'
+    
+print 'a b  c'.split(' ')
+
+print re.split(r'\s+', 'a b  c')
+
+print re.split(r'[\s\,]+', 'a,b, c  d')
+
+print re.split(r'[\s\,\:]+', 'a,b:: c d')
+
+m = re.match(r'^(\d{3})-(\d{3,8})$', '010-12345')
+print m.group(0)
+print m.group(1)
+print m.group(2)
+
+t = '19:05:30'
+m = re.match(r'^(0[0-9]|1[0-9]|2[0-3]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])\:(0[0-9]|1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|[0-9])$', t)
+print m.groups()
+
+print re.match(r'^(\d+)(0*)$', '102300').groups()
+print re.match(r'^(\d+?)(0*)$', '102300').groups()
+
+
+re_telephone = re.compile(r'^(\d{3})-(\d{3,8})$')
+print re_telephone.match('010-12345').groups()
+print re_telephone.match('010-8086').groups()
