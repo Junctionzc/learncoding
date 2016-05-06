a4f2b577504e78c289f8a24fb560b160b5d1bee2
Junctionzc <junctionzc@gmail.com>
2016-05-06 08:46:43 +0800

    learn data structures day 2

---
+++ data_structure_and_algorithms/book/DSAC/chapter3/example/test.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "list.h"
+
+void ListTest();
+
+int main(int argc, char **argv)
+{
+    ListTest();
+    
+    return 0;
+}
+
+void ListTest()
+{
+    struct Node node;
+    List L = &node;
+    Position P;
+    
+    L->Next = NULL;
+    
+    printf("Is list empty? %s\n", IsEmpty(L) ? "Yes." : "No.");
+    
+    Insert(5, L, L);
+    
+    printf("Have inserted an element 5 into the list.\n");
+    
+    printf("Is list empty? %s\n", IsEmpty(L) ? "Yes." : "No.");
+    
+    P = Find(5, L);
+    
+    if (P != NULL) {
+        printf("There is an element %d in list.\n", P->Element);
+    } else {
+        printf("Couldn't find element 5 in list.\n");
+    }
+    
+    Delete(5, L);
+    
+    printf("Have deleted the element 5.\n");
+    
+    printf("Is list empty? %s\n", IsEmpty(L) ? "Yes." : "No.");
+}
