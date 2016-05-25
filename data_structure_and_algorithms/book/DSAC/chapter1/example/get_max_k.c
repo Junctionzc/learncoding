5f4b89164387082b64a0443f334dcd25769df968
Junctionzc <junctionzc@gmail.com>
2016-05-25 22:11:52 +0800

    just backup

---
+++ data_structure_and_algorithms/book/DSAC/chapter1/example/get_max_k.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+typedef int ElementType;
+
+void bubble_sort(ElementType *array, int array_len)
+{
+    int i, j;
+    int tmp;
+    
+    for (i = 0; i < array_len, ++i) {
+        for (j = 0; j < ; ++j) {
+            
+        }
+    }
+}
+
+int main()
+{
+    return 0;
+}
