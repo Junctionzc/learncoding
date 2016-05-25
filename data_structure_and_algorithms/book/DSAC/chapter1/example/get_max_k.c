c0aba099825f6dc2347a5f922562b7bea9b56eca
Junctionzc <junctionzc@gmail.com>
2016-05-26 00:17:43 +0800

    add bubble sort but not test.

--- data_structure_and_algorithms/book/DSAC/chapter1/example/get_max_k.c
+++ data_structure_and_algorithms/book/DSAC/chapter1/example/get_max_k.c
@@ -2,19 +2,24 @@
 
 typedef int ElementType;
 
-void bubble_sort(ElementType *array, int array_len)
+void bubble_sort(ElementType *p_array, int array_len)
 {
     int i, j;
-    int tmp;
+    ElementType tmp;
     
-    for (i = 0; i < array_len, ++i) {
-        for (j = 0; j < ; ++j) {
-            
+    for (i = 0; i < array_len; ++i) {
+        for (j = 0; j < array_len - i - 1; ++j) {
+            if (p_array[j] < p_array[j + 1]) {
+                tmp = p_array[j];
+                p_array[j] = p_array[j + 1];
+                p_array[j + 1] = tmp;
+            }
         }
     }
 }
 
 int main()
 {
+    int array
     return 0;
 }
