e42fad9f62411665d66e518e4dc6df152843ca6c
Junctionzc <junctionzc@gmail.com>
2016-06-03 10:07:42 +0800

    add python user password example

---
+++ python/book/CorePythonProgramming/userpw.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+db = {}
+
+def newuser():
+    prompt = 'login desired: '
+    while True:
+        name = raw_input(prompt)
+        if db.has_key(name):
+            prompt = 'name taken, try another: '
+            continue
+        else:
+            pwd = raw_input('passwd: ')
+            db[name] = pwd
+            break
+        
+def olduser():
+    name = raw_input('login: ')
+    pwd = raw_input('passwd: ')
+    passwd = db.get(name)
+    if passwd == pwd:
+        print 'welcome back', name
+    else:
+        print 'login incorrect'
+        
+def showmenu():
+    prompt = """
+(N)ew User Login
+(E)xisting User Login
+(Q)uit
+
+Enter choice: """
+
+    done = False
+    while not done:
+        chosen = False
+        while not chosen:
+            try:
+                choice = raw_input(prompt).strip()[0].lower()
+            except (EOFError, KeyboardInterrupt):
+                choice = 'q'
+            print '\nYou picked: [%s]' % choice
+            if choice not in 'neq':
+                print 'invalid option, try again'
+            else:
+                chosen = True
+                done = True
+                
+                newuser()
+                olduser()
+    
+if __name__ == '__main__':
+    showmenu()
